3ab4802a830ad44a0d94eec6cb02dfefa0b1a72d
Keith Fernandes <145413230+keithfernz30@users.noreply.github.com>
2025-11-19 15:47:38 +0530

    Create Evaluate Reverse Polish Notation .py

---
+++ Stacks /Evaluate Reverse Polish Notation .py
@@ -0,0 +1,22 @@
+from math import ceil , floor
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack: List[int] = []
+
+        for t in tokens:
+            if t in "+-*/":
+                b = stack.pop()
+                a = stack.pop()
+
+                if t == "+":
+                    stack.append(a + b)
+                elif t == "-":
+                    stack.append(a - b)
+                elif t == "*":
+                    stack.append(a * b)
+                else:  
+                    stack.append(int(a / b))
+            else:
+                stack.append(int(t))
+
+        return stack[0]
